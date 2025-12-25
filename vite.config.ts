fe7dcc7c58a2aa25203f09068c85fe9408898361
wuyula1122 <121240805+wuyula1122@users.noreply.github.com>
2025-12-25 15:34:36 +0800

    Set base path for GitHub repository in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 必须添加这一行，填入你的 GitHub 仓库名
+  base: '/sanchuan-album/', 
 })
